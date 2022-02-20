fa5c31cdbf245148e7a3d45d3637375096cb151a
max1mp <m.pohribniak@weplay.tv>
2022-02-20 21:38:49 +0200

    Fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-const colors = require('tailwindcss/colors')
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false,
     theme: {
         extend: {},
